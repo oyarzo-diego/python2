c4b5f8ebedd36c34c846645f2754b1e76e6a5f71
oyarzo-diego <doyarzo2016@udec.cl>
2025-03-19 18:15:26 -0300

    clase1_v1
    Esta version nosotros aprendimos el print

---
+++ clase1.py
@@ -0,0 +1,13 @@
+# comentario 1
+#* comentario 2
+#! comentario 3
+#? comentario 4
+
+#! Este script corresponde a la primera clase de la asigantura de python
+
+#? p = 2.7
+#? pcu = 4.5
+
+
+print('Hola mundo')
+
